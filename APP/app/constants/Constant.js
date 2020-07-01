8ecaf840786176b61352a32463579b9622c6cf1e
hoang <neckgamervn@gmail.com>
2020-07-01 21:51:56 +0700

    fixx

--- APP/app/constants/Constant.js
+++ APP/app/constants/Constant.js
@@ -5,12 +5,12 @@ module.exports = {
   },
   SCREEN_ROUTER_AUTH: {
     LOGIN: "Login",
-    AUTH_LOADING: "AuthLoading",
     FORGOT_PASS: "ForgotPassword",
     REGISTER: "Register"
   },
   SCREEN_ROUTER_APP: {
     HOME: "Home",
     USER: "User"
-  }
+  },
+  AUTH_LOADING: "AuthLoading"
 };
--- APP/app/navigation/AppNavigator.js
+++ APP/app/navigation/AppNavigator.js
@@ -4,21 +4,21 @@ import AuthLoadingScreen from "@app/screens/auth/AuthLoadingScreen";
 import LoginScreen from "@app/screens/auth/LoginScreen";
 import RegisterScreen from "@app/screens/auth/RegisterScreen";
 import ForgotPasswordScreen from "@app/screens/auth/ForgotPasswordScreen";
-import { SCREEN_ROUTER } from "@constant";
+import { SCREEN_ROUTER, SCREEN_ROUTER_AUTH, AUTH_LOADING } from "@constant";
 import Main from "./MainTabNavigator";
 
 const Auth = createStackNavigator(
   {
-    [SCREEN_ROUTER.LOGIN]: LoginScreen,
-    [SCREEN_ROUTER.REGISTER]: RegisterScreen,
-    [SCREEN_ROUTER.FORGOT_PASS]: ForgotPasswordScreen
+    [SCREEN_ROUTER_AUTH.LOGIN]: LoginScreen,
+    [SCREEN_ROUTER_AUTH.REGISTER]: RegisterScreen,
+    [SCREEN_ROUTER_AUTH.FORGOT_PASS]: ForgotPasswordScreen
   },
   { headerMode: "none" }
 );
 export default createAppContainer(
   createSwitchNavigator(
     {
-      [SCREEN_ROUTER.AUTH_LOADING]: AuthLoadingScreen,
+      [AUTH_LOADING]: AuthLoadingScreen,
       [SCREEN_ROUTER.AUTH]: Auth,
       [SCREEN_ROUTER.MAIN]: Main
     },
